deb5af6606a2e214a210be4765eae64cb3a20c49
Alyssa Holzer <holzealy@uw.edu>
2017-05-31 23:44:03 -0700

    show line and area at different steps

--- js/script.js
+++ js/script.js
@@ -9,13 +9,17 @@ $(function () {
             d3.select('.curve').style('opacity', 0);
         } else if (document.body.scrollTop > 100 && document.body.scrollTop < 600) {
             d3.select('.curve').style('opacity', 1);
-            d3.select('.area').style('opacity', 0);
             d3.select('.vertical').style('opacity', 0);
-        } else if (document.body.scrollTop > 600 && document.body.scrollTop < 1700) {
+            d3.select('.area').style('opacity', 0);
+        } else if (document.body.scrollTop > 600 && document.body.scrollTop < 1300) {
             d3.select('.curve').style('opacity', 1);
-            d3.select('.area').style('opacity', 1);
             d3.select('.vertical').style('opacity', 1);
-        } else if (document.body.scrollTop > 1700) {
+            d3.select('.area').style('opacity', 0);
+        } else if (document.body.scrollTop > 1300 && document.body.scrollTop < 1800) {
+            d3.select('.curve').style('opacity', 1);
+            d3.select('.vertical').style('opacity', 1);
+            d3.select('.area').style('opacity', 1);
+        } else if (document.body.scrollTop > 1800) {
             d3.select('.curve').style('opacity', 0);
         }
     }
